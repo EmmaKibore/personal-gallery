91ea045b53e9bdc296a417b5ae1edb63e0dc4fc8
EmmaKibore <emmakibore@gmail.com>
2019-05-14 13:31:47 +0300

    added an image

--- images/urls.py
+++ images/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns=[
-    url('^$',views.index,name = 'Images'),
+    url('^$',views.index,name = 'index'),
     url(r'^search/', views.search_page, name='search_page'),
     url(r'^image/(\d+)', views.single_image, name = 'single_image'),
     url(r'^locations/sorted/$', views.sortby_locations, name = 'sortby_locations'),
